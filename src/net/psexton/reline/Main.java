8787a2fecb137f17483d1c588dba3d7f69461330
Paul Sexton <psexton@gnresound.com>
2014-05-14 12:01:32 -0500

    Adding NetBeans project scaffolding.

---
+++ src/net/psexton/reline/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Main.java, part of the reline project
+ * Created on May 14, 2014, 11:50:42 AM
+ * Paul Sexton
+ */
+
+package net.psexton.reline;
+
+/**
+ *
+ * @author PSexton
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
