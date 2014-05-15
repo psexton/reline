23e34e6314f32a2de4be7155bb1b80a690fe5d6c
Paul Sexton <psexton@gnresound.com>
2014-05-15 13:45:15 -0500

    GPL headers
    - Telling NetBeans this is a GPL3 project.
    - Adding custom license template file to project.
    - Adding GPL3 header to existing files.

--- src/net/psexton/reline/MainFrame.java
+++ src/net/psexton/reline/MainFrame.java
@@ -1,6 +1,20 @@
 /*
  * MainFrame.java, part of the reline project
  * Created on May 14, 2014, 12:02:03 PM
+ * Copyright (C) 2014 PSexton
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package net.psexton.reline;
