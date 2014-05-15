405403fff6a77eea55487122f6aa22fe0b638ac9
Paul Sexton <psexton@gnresound.com>
2014-05-15 13:16:13 -0500

    Trying to hack it by hand. Unsuccesfully so far.

--- src/net/psexton/reline/Model.java
+++ src/net/psexton/reline/Model.java
@@ -19,8 +19,15 @@
 
 package net.psexton.reline;
 
+import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.text.DateFormat;
 import java.util.Calendar;
 import javax.swing.JTextArea;
@@ -31,16 +38,24 @@ import javax.swing.Timer;
  * @author PSexton
  */
 public class Model {
+    private static final int JOIN_REGION_WIDTH = 142;
+    private static final int RESTART_REGION_WIDTH = 254;
+    private static final int REGION_HEIGHT = 3;
+    private static final int WHITE = new Color(255, 255, 255).getRGB();
+    private static final int GREEN = new Color(69, 173, 58).getRGB();
+    
     private JTextArea console;
     private boolean isRunning;
     private Timer timer;
-    DateFormat dateFormat;
+    private final DateFormat dateFormat;
+    private Robot robot;
     
     public Model() {
         console = null;
         isRunning = false;
         timer = null;
         dateFormat = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM);
+        robot = null;
     }
     
     public void startMonitor(int intervalInSeconds, JTextArea console) {
@@ -57,7 +72,15 @@ public class Model {
                 monitor();
             }
         });
-        timer.start();
+        try {
+            robot = new Robot();
+            timer.setInitialDelay(0); // trigger immediately upon starting
+            //timer.setRepeats(false);
+            timer.start();
+        } 
+        catch (AWTException ex) {
+            console.setText(ex.toString());
+        }
     }
     
     public void stopMonitor() {
@@ -70,7 +93,25 @@ public class Model {
     }
     
     private void monitor() {
-        appendLine("Checking");
+        appendLine("Checking...");
+        BufferedImage screenShot = robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+        appendLine("\tGot screenshot");
+        Point joinClickPoint = findGreenSubregion(screenShot, JOIN_REGION_WIDTH, REGION_HEIGHT);
+        if(joinClickPoint != null) {
+            appendLine("\tFound join button");
+            robot.mouseMove(joinClickPoint.x, joinClickPoint.y);
+        }
+        else {
+            appendLine("\tDid not find join button");
+            Point restartClickPoint = findGreenSubregion(screenShot, RESTART_REGION_WIDTH, REGION_HEIGHT);
+            if(restartClickPoint != null) {
+                appendLine("\tFound restart button");
+                robot.mouseMove(restartClickPoint.x, restartClickPoint.y);
+            }
+            else {
+                appendLine("\tDid not find restart button");
+            }
+        }
     }
     
     /**
@@ -78,7 +119,64 @@ public class Model {
      * @param contents 
      */
     private void appendLine(String content) {
-        String dateAndTimePrefix = "[" + dateFormat.format(Calendar.getInstance().getTime()) + "]";
-        console.append(dateAndTimePrefix + " " + content + "\n");
+        appendLine(content, false);
+    }
+    private void appendLine(String content, boolean noPrefix) {
+        String dateAndTimePrefix = noPrefix ? "" : "[" + dateFormat.format(Calendar.getInstance().getTime()) + "] ";
+        console.append(dateAndTimePrefix + content + "\n");
+    }
+    
+    /**
+     * 
+     * @param large The larger image to look in
+     * @return Point with top left coordinates, or null
+     */
+    private Point findGreenSubregion(BufferedImage image, int regionWidth, int regionHeight) {
+        /*
+         * Rather than actually search for the images, we vastly simplify:
+         * For each button we look for a Nx3 region.
+         * The first row will be all white pixels.
+         * The second and third rows will be white pixels at the ends, and green
+         * pixels in all interior locations.
+         * 
+         * We will search until we find a matching subregion, or run out of screen.
+         * If we do find a match, return the location in the original image of the
+         * top left green pixel (second row, second column in the subregion).
+         */
+        
+        // Iterating over all possible start points
+        int minStartX = image.getMinX();
+        int minStartY = image.getMinY();
+        int maxStartX = image.getWidth() - regionWidth - 1;
+        int maxStartY = image.getHeight() - regionHeight - 1;
+        for(int y = minStartY; y <= maxStartY; y++) {
+            for(int x = minStartX; x <= maxStartX; x++) {
+                BufferedImage subImage = image.getSubimage(x, y, regionWidth, regionHeight);
+                if(isMatchingSubRegion(subImage))
+                    return new Point(x, y);
+            }
+        }
+        
+        return null;
+    }
+    
+    private boolean isMatchingSubRegion(BufferedImage image) {
+        // First row should be all white
+        final int maxX = image.getWidth() - 1;
+        for(int x = 0; x <= maxX; x++) {
+            if(image.getRGB(x, 0) != WHITE)
+                return false;
+        }
+        // Second and third rows should be white on edges, and green in interior
+        for(int y = 1; y <= 2; y++) {
+            if(image.getRGB(0, y) != WHITE || image.getRGB(maxX, y) != WHITE)
+                return false;
+            for(int x = 1; x < maxX; x++) {
+                if(image.getRGB(x, y) != GREEN)
+                    return false;
+            }
+        }
+        
+        return true;
     }
 }
